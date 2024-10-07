8b2d7fa126ba0cf39d242e6637b12d2829f04326
Hilwa Isnaini Marfuah <isnainihilwa19@gmail.com>
2024-10-07 20:45:17 +0700

    Add project files.

---
+++ Biodata.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace pertemuan9
+{
+    partial class Biodata
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nama = new System.Windows.Forms.Label();
+            this.gender = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.hobi = new System.Windows.Forms.Label();
+            this.ngoding = new System.Windows.Forms.CheckBox();
+            this.ngegame = new System.Windows.Forms.CheckBox();
+            this.turu = new System.Windows.Forms.CheckBox();
+            this.rbPria = new System.Windows.Forms.RadioButton();
+            this.rbWanita = new System.Windows.Forms.RadioButton();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nama
+            // 
+            this.nama.AutoSize = true;
+            this.nama.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nama.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.nama.Location = new System.Drawing.Point(101, 72);
+            this.nama.Name = "nama";
+            this.nama.Size = new System.Drawing.Size(201, 55);
+            this.nama.TabIndex = 0;
+            this.nama.Text = "NAMA : ";
+            // 
+            // gender
+            // 
+            this.gender.AutoSize = true;
+            this.gender.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gender.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.gender.Location = new System.Drawing.Point(101, 159);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(417, 55);
+            this.gender.TabIndex = 1;
+            this.gender.Text = "JENIS KELAMIN : ";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNama.Location = new System.Drawing.Point(292, 85);
+            this.txtNama.Multiline = true;
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(356, 42);
+            this.txtNama.TabIndex = 2;
+            // 
+            // hobi
+            // 
+            this.hobi.AutoSize = true;
+            this.hobi.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hobi.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hobi.Location = new System.Drawing.Point(101, 243);
+            this.hobi.Name = "hobi";
+            this.hobi.Size = new System.Drawing.Size(185, 55);
+            this.hobi.TabIndex = 4;
+            this.hobi.Text = "HOBI : ";
+            // 
+            // ngoding
+            // 
+            this.ngoding.AutoSize = true;
+            this.ngoding.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ngoding.Location = new System.Drawing.Point(292, 250);
+            this.ngoding.Name = "ngoding";
+            this.ngoding.Size = new System.Drawing.Size(208, 49);
+            this.ngoding.TabIndex = 5;
+            this.ngoding.Text = "NGODING";
+            this.ngoding.UseVisualStyleBackColor = true;
+            // 
+            // ngegame
+            // 
+            this.ngegame.AutoSize = true;
+            this.ngegame.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ngegame.Location = new System.Drawing.Point(293, 305);
+            this.ngegame.Name = "ngegame";
+            this.ngegame.Size = new System.Drawing.Size(207, 49);
+            this.ngegame.TabIndex = 6;
+            this.ngegame.Text = "NGEGAME";
+            this.ngegame.UseVisualStyleBackColor = true;
+            // 
+            // turu
+            // 
+            this.turu.AutoSize = true;
+            this.turu.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turu.Location = new System.Drawing.Point(293, 360);
+            this.turu.Name = "turu";
+            this.turu.Size = new System.Drawing.Size(136, 49);
+            this.turu.TabIndex = 7;
+            this.turu.Text = "TURU";
+            this.turu.UseVisualStyleBackColor = true;
+            // 
+            // rbPria
+            // 
+            this.rbPria.AutoSize = true;
+            this.rbPria.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbPria.Location = new System.Drawing.Point(504, 165);
+            this.rbPria.Name = "rbPria";
+            this.rbPria.Size = new System.Drawing.Size(122, 49);
+            this.rbPria.TabIndex = 8;
+            this.rbPria.TabStop = true;
+            this.rbPria.Text = "PRIA";
+            this.rbPria.UseVisualStyleBackColor = true;
+            // 
+            // rbWanita
+            // 
+            this.rbWanita.AutoSize = true;
+            this.rbWanita.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbWanita.Location = new System.Drawing.Point(662, 165);
+            this.rbWanita.Name = "rbWanita";
+            this.rbWanita.Size = new System.Drawing.Size(189, 49);
+            this.rbWanita.TabIndex = 9;
+            this.rbWanita.TabStop = true;
+            this.rbWanita.Text = "WANITA";
+            this.rbWanita.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLogin.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnLogin.Location = new System.Drawing.Point(367, 498);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(208, 72);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Biodata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(978, 656);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.rbWanita);
+            this.Controls.Add(this.rbPria);
+            this.Controls.Add(this.turu);
+            this.Controls.Add(this.ngegame);
+            this.Controls.Add(this.ngoding);
+            this.Controls.Add(this.hobi);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.nama);
+            this.Name = "Biodata";
+            this.Text = "Biodata";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nama;
+        private System.Windows.Forms.Label gender;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label hobi;
+        private System.Windows.Forms.CheckBox ngoding;
+        private System.Windows.Forms.CheckBox ngegame;
+        private System.Windows.Forms.CheckBox turu;
+        private System.Windows.Forms.RadioButton rbPria;
+        private System.Windows.Forms.RadioButton rbWanita;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Biodata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pertemuan9
+{
+    public partial class Biodata : System.Windows.Forms.Form
+    {
+        public Biodata()
+        {
+            InitializeComponent();
+        }
+    }
+}
